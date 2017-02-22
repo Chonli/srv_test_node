639aa294f9ed6ad27bc7958bfbc5654c0c5d0211
Chonli <Chonli@users.noreply.github.com>
2017-02-22 09:19:55 +0100

    Improve log trace with add timestamp

--- node_srv_send_big_file.js
+++ node_srv_send_big_file.js
@@ -1,18 +1,19 @@
 var net = require('net');
 const fs = require('fs');
+var timestamp = require('console-timestamp');
 
 const file_send = 'file_to_send';
 
 var server = net.createServer();
 server.on('connection', handleConnection);
 
-server.listen(1234, "0.0.0.0", function() {
-  console.log('server listening to %j', server.address());
+server.listen(5000, "0.0.0.0", function() {
+  console.log('DD-MM-YY hh:mm:ss : '.timestamp + 'server listening to %j', server.address());
 });
 
 function handleConnection(conn) {
   var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
-  console.log('new client connection from %s', remoteAddress);
+  console.log('DD-MM-YY hh:mm:ss : '.timestamp + 'new client connection from %s', remoteAddress);
 
   conn.on('data', onConnData);
   conn.once('close', onConnClose);
@@ -23,11 +24,11 @@ function handleConnection(conn) {
   }
 
   function onConnClose() {
-    console.log('Connection from %s closed', remoteAddress);
+    console.log('DD-MM-YY hh:mm:ss : '.timestamp + 'Connection from %s closed', remoteAddress);
   }
 
   function onConnError(err) {
-    console.log('Connection %s error: %s', remoteAddress, err.message);
+    console.log('DD-MM-YY hh:mm:ss : '.timestamp + 'Connection %s error: %s', remoteAddress, err.message);
   }
 
   //send file to client
@@ -36,20 +37,23 @@ function handleConnection(conn) {
   var read_index = 0;
   var fileStream = fs.createReadStream(file_send);
   fileStream.on('error', function(err) {
-    console.log(err);
+    console.log('DD-MM-YY hh:mm:ss'.timestamp + err);
     conn.end();
   });
 
   fileStream.on('data', (chunk) => {
     read_length += chunk.length;
-    console.log(`${++read_index} - read ${read_length} bytes of ${total_length}.`);
+
+    console.log(`${++read_index} - ` + 'DD-MM-YY hh:mm:ss : '.timestamp + ` - read ${read_length} bytes of ${total_length}.`);
   });
 
   fileStream.on('open', function() {
+	console.time('Send time');
     fileStream.pipe(conn);
   });
 
   fileStream.on('end', function() {
-    console.log(`End to send file ${file_send}`);
+	console.timeEnd('Send time');
+    console.log('DD-MM-YY hh:mm:ss : '.timestamp + `End to send file ${file_send}`);
   });
 }
